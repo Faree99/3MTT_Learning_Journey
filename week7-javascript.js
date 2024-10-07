a6e566ca16f4176309f78ece838e4924171c17b4
Faree99 <adebayofaridah99@gmail.com>
2024-10-07 21:15:38 +0100

    Updated

---
+++ week7-javascript.js
@@ -0,0 +1,47 @@
+let x= 25;
+x=10;
+var y=22;
+const z= x + y;
+const test="Hello world!";
+console.log(x);
+console.log(y);
+console.log(typeof z);
+console.log(typeof test);
+
+const alph= typeof test;
+const alph2= typeof test;
+const num= typeof y;
+const num2= typeof y;
+
+
+console.log(alph);
+console.log(num);
+
+
+
+
+let a=6;
+let b=2;
+let check= a===b;
+console.log(check);
+console.log(typeof check);
+
+let chek1;
+console.log(chek1);
+
+// if(alph === num) {
+//     alert("It's a string");   
+// } else{
+//     alert("It's a not");   
+// }
+console.log(typeof String);
+
+
+if(alph && alph2 === "string") {
+    alert("yes");   
+} else{
+    alert("nah");   
+}
+
+
+
